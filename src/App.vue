6eb1f04ea361ca7a155b4813d50ba7d3db8b9c60
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-04-26 14:23:44 +0300

    addNote

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,48 @@
+<script setup>
+import { ref } from 'vue'
+
+const showModal = ref(false)
+const newNote = ref('')
+const notes = ref([])
+
+const getRandomColor = () => {
+  return 'hsl(' + Math.random() * 360 + ', 100%, 75%)'
+}
+
+const addNote = () => {
+  notes.value.push({
+    id: Math.floor(Math.random() * 1000),
+    text: newNote.value,
+    date: new Date().toLocaleString(),
+    backgroundColor: getRandomColor()
+  })
+  showModal.value = false
+  newNote.value = ''
+}
+</script>
+
 <template>
   <main>
+    <div v-show="showModal" class="overlay">
+      <div class="modal">
+        <textarea v-model="newNote" name="note" id="note" cols="30" rows="10"></textarea>
+        <div class="modalBtns">
+          <button @click="addNote">Add Note</button>
+          <button @click="showModal = false" class="close">x</button>
+        </div>
+      </div>
+    </div>
     <div class="container">
       <header>
         <h1>Notes</h1>
-        <button>Add</button>
+        <button @click="showModal = true">Add</button>
       </header>
       <div class="cards-container">
         <div class="card">
-          <p class="main-text"></p>
-          <p class="date"></p>
+          <p class="main-text">
+            {{ cardText }}
+          </p>
+          <p class="date">{{ cardDate }}</p>
         </div>
       </div>
     </div>
@@ -29,5 +63,99 @@ main {
 header {
   display: flex;
   justify-content: space-between;
+  align-items: center;
+}
+
+h1 {
+  font-weight: bold;
+  margin-bottom: 25px;
+  font-size: 55px;
+}
+
+header button {
+  font-size: 20px;
+  padding: 10px;
+  border-radius: 10px;
+  cursor: pointer;
+}
+
+button:hover {
+  box-shadow: 0 0 10px 5px grey;
+}
+
+button:active {
+  transform: translateY(5px);
+  transition: 0.3s ease-in-out;
+}
+
+.cards-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+}
+
+.card {
+  width: 225px;
+  height: 225px;
+  background-color: rgb(237, 182, 44);
+  padding: 10px;
+  border-radius: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-right: 20px;
+  margin-bottom: 20px;
+}
+
+.date {
+  font-size: 12px;
+  font-weight: bold;
+}
+
+.overlay {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.77);
+  z-index: 10;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal {
+  width: 750px;
+  background-color: white;
+  border-radius: 10px;
+  padding: 30px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+}
+
+.modal textarea {
+  border-radius: 15px;
+  margin-bottom: 20px;
+  height: 200px;
+}
+
+.modal button {
+  width: 200px;
+  margin: 0 auto;
+  border-radius: 10px;
+  padding: 5px;
+  font-size: 20px;
+  cursor: pointer;
+  color: white;
+  background-color: blueviolet;
+  border: 1px ridge;
+}
+
+.modal .close {
+  background-color: rgb(136, 17, 17);
+}
+
+.modalBtns {
+  display: flex;
 }
 </style>
