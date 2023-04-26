c1840a671b3c28b21b8806bc44113edd95a23c02
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-04-26 12:19:09 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <main>
+    <div class="container">
+      <header>
+        <h1>Notes</h1>
+        <button>Add</button>
+      </header>
+      <div class="cards-container">
+        <div class="card">
+          <p class="main-text"></p>
+          <p class="date"></p>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  height: 100vh;
+  width: 100vw;
+}
+.container {
+  max-width: 1000px;
+  padding: 10px;
+  margin: 0 auto;
+}
+
+header {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
